6ff56aeff2a6a6ec31dfbb281cb0c08007f5a0b1
antoni1913 <antonisariev13@gmail.com>
2023-09-17 17:05:28 +0300

    week 2

---
+++ week2ifstatements/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+
+Console.WriteLine("Enter your last name:");
+string lastName = Console.ReadLine();
+
+Console.WriteLine("Enter your gender (f/m/n):");
+char userGender = char.Parse(Console.ReadLine());
+
+string greeting;
+
+ if(userGender == 'f')
+{
+    greeting = "Ms.";
+}else if (userGender == 'm') {
+    greeting = "Mr.";
+}else
+{
+    greeting = "";
+}
+
+Console.WriteLine($"Hello, {greeting} {lastName}!");
+
+Console.WriteLine("Enter your year of birth:");
+
+int usersYB = Int32.Parse(Console.ReadLine());
+
+int currentYear = DateTime.Now.Year;
+
+int userAge = currentYear - usersYB;
+
+
+
+
+//int userAge = Int32.Parse(Console.ReadLine());
+
+if (userAge >= 13)
+{
+    Console.WriteLine($"You are {userAge} old enought to have an account");
+}
+else if (userAge < 13)
+{
+    Console.WriteLine($"You are {userAge} NO old enought to have an account");
+}
+
